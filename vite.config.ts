10dacbab841a820d347c6fa392f2c9883c2d805e
Eduardo Morales <emoral435@gmail.com>
2023-02-23 21:23:22 -0600

    Deploy Hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Shed_Shopping'
+  base: '/Shed_Shopping/',
+  plugins: [react()]
 })
